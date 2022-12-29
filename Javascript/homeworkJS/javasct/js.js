74ccc1957ea60483ab9fe57f623606654e6388ca
Timur <t.akanov@a-i.kz>
2022-12-29 22:14:25 +0600

    Home 4

---
+++ Javascript/homeworkJS/javasct/js.js
@@ -0,0 +1,206 @@
+// HOME WORK
+
+// 1
+// alert('Учу!');
+
+
+
+// 2
+// let a = 2;
+// let b = 3;
+// let c = 4;
+
+
+// 3
+
+
+// let a = Number(prompt('Введите число'));
+
+// alert(a **2);
+
+// 4
+
+// let numberOne = Number(prompt('Введите первое число'));
+// let numberTwo = Number(prompt('Введите второе число'));
+
+// alert(numberOne + numberTwo);
+
+// 5
+
+// let num1 = 15;
+// let num2 = num1;
+
+// console.log(num2);
+// alert(num2);
+
+
+// 6 
+
+
+// 6/1
+// let a = 3;
+// let b = 10;
+// let c = 9;
+// alert(a*b*c);
+
+// 6/2
+// let a = 4;
+// let b = 5;
+// let c = 15;
+// alert(a*b*c);
+
+
+
+//// 7
+
+// let a = 4;
+// let b = 2;
+// let c = 10;
+//  alert((a+b)/2 + (b+c)/2);
+
+
+// 8 
+
+// let numberEnter = Number(prompt('Введите число 4'));
+
+// alert((((numberEnter + 25)/ 10) - 12) ** 2);
+
+
+// 9
+
+
+// let a = 17;
+// let b = 10;
+// let c = a-b;
+// let d = 7;
+
+// let result = c+ d;
+// alert(result);
+
+
+// 10
+
+//  let a = 4;
+//  let b = 2;
+//  let c = a%b;
+//  alert(c);
+ 
+
+//  let a = 12;
+//  let b = 5;
+//  let c = a % b;
+//  alert(c);
+
+
+
+// 11
+
+// let hours = Number(prompt('Введите часы'));
+// let minuts = Number(prompt('Введите минуты'));
+// let second = Number(prompt('Введите секунды'));
+
+// alert((hours) + ' : ' + (minuts) + ' : ' + (second));
+
+
+// 12 
+// let a =  5;
+// let b = 4;
+
+// [a , b]=[b , a];
+// console.log('a = ' + ' ' + a, 'b =' + '' + b);
+
+
+//13
+
+// let str= 'abcde';
+
+// alert();
+
+    // let str = 'abcde';
+    // alert(str[0]);
+    // alert(str[1]);
+    // alert(str[4]);
+//14
+
+
+// let a = Number(prompt('Введите первое число'));
+// let b = Number(prompt('Введите второе число'));
+
+// alert('Квадрат гипотенузы = ' + (a **2 + b ** 2));
+
+
+// 15 
+
+// let age = Number(prompt('Введите свой возраст'));
+
+// alert(' Мне ' + age + ' лет ');
+
+// 16
+
+
+//let whatIsName = prompt('Введите свое Имя');
+
+//alert(' Welcome ' + whatIsName);
+
+
+// 17
+
+
+// let numberOne = Number(prompt('Введите первое число'));
+// let numberTwo = Number(prompt('Введите второе число'));
+// let numberThere = Number(prompt('Введите третие число'));
+
+// alert(' Разница трех чисел = '+ Math.abs(numberOne - numberTwo - numberThere));
+
+
+// 18
+
+// let a = Number(prompt('Enter Number One'));
+// let b = Number(prompt('Enter Number Two'));
+// let c = Number(prompt(' Enter Nuber There'));
+
+// alert(' Сумма трех чисел равна '+((a+2) + (b-3) + (c**2)));
+
+
+
+// 19
+
+// let temperature = Number(prompt('Введите температуру в градусах Цельсия'));
+
+// alert(' Градусах Фаренгейта = ' + ((9/5) * temperature + 32));
+
+
+// 20
+
+// let a = Number(prompt('Введите  число а'));
+// let b = Number(prompt('Введите число в'));
+// let c = Number(prompt('Введите число с'));
+
+// alert((a**2)+(3*b**3) + (3*a*b) + (c**2));
+
+// let a = Number(prompt('Введите  число а'));
+// let b = Number(prompt('Введите число в'));
+// let c = Number(prompt('Введите число с'));
+
+// alert((Math.row(a,2))+ (3 * (Math.row(b,3))) + (3*a*b) + (Math.row(c,2)));
+
+//console.log(Math.row( 4 , 2 ));
+
+// console.log(Math.row(6 , 8));
+
+
+// Способ при помощи (pow)
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// alert(Math.pow(a,2) + (3*Math.pow(b,3))+ (3*a*b)+ (Math.pow(c,2)));
+// a2 + 3b3 + 3ab + c2
+
+
+
+
+
+
+
+
